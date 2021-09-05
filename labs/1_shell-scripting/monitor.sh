e42d4d3a7a5d756078087536a557e3a9b87db970
Mikey <54919721+mikebrownie@users.noreply.github.com>
2021-09-05 17:42:28 -0600

    Update monitor.sh

--- labs/1_shell-scripting/monitor.sh
+++ labs/1_shell-scripting/monitor.sh
@@ -1,8 +1,18 @@
-!#/bin/bash
+#!#/bin/bash
 
-ROOT_USAGE=$(df | grep "/$" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//')
+ROOT_USAGE=$(df | grep "/$" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//');
 
-BOOT_USAGE=$(df | grep "/boot" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//')
+BOOT_USAGE=$(df | grep "/boot" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//');
 
+i=0;
 
-
+while [ $i -lt 10 ]
+do
+  if [ $BOOT_USAGE -lt 80 ]
+  then
+    echo "Uh oh";
+  if [ ROOT_USAGE -lt 80 ]
+  then
+    echo "Uh oh";
+  fi
+  i=`expr $i + 1`;
