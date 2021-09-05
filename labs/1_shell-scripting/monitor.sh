3c6c3abacbc771c3f3446986b7e2061f70d9eb23
Mikey <54919721+mikebrownie@users.noreply.github.com>
2021-09-05 17:58:50 -0600

    Need to add mail

--- labs/1_shell-scripting/monitor.sh
+++ labs/1_shell-scripting/monitor.sh
@@ -1,18 +1,21 @@
-#!#/bin/bash
+#!/usr/bin/env bash
+ROOT_USAGE=$(df | grep "/$" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//')
 
-ROOT_USAGE=$(df | grep "/$" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//');
+BOOT_USAGE=$(df | grep "/boot" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//')
 
-BOOT_USAGE=$(df | grep "/boot" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//');
+i=0
 
-i=0;
-
-while [ $i -lt 10 ]
+while [ $i -lt 1000 ]
 do
-  if [ $BOOT_USAGE -lt 80 ]
+  if [ $BOOT_USAGE -gt 80 ]
   then
-    echo "Uh oh";
-  if [ ROOT_USAGE -lt 80 ]
-  then
-    echo "Uh oh";
+    echo "Uh oh"
   fi
+  if [ $ROOT_USAGE -gt 80 ]
+  then
+    echo "Uh oh"
+  fi
+  sleep 15
   i=`expr $i + 1`;
+done;
+
