03b5e3a987f5c2b8adcffa1365c558ed999515a4
Mikey <54919721+mikebrownie@users.noreply.github.com>
2021-09-06 09:20:30 -0600

    Added network interface

--- labs/1_shell-scripting/dashboard.sh
+++ labs/1_shell-scripting/dashboard.sh
@@ -9,4 +9,23 @@ display_mem_res()
   echo "Free RAM: $ram MB"
 }
 
+
+display_net_info()
+{
+  echo 'NETWORK CONNECTIONS -------------------------------------'
+  rec=$(cat /proc/net/dev | awk 'FNR == 4 {print $2}')
+  sent=$(cat /proc/net/dev | awk 'FNR == 4 {print $10}')
+  echo "lo Bytes Received: $rec Transmitted: $sent"
+
+  rec=$(cat /proc/net/dev | awk 'FNR == 3 {print $2}')
+  sent=$(cat /proc/net/dev | awk 'FNR == 3 {print $10}')
+  echo "enp0s3 Bytes Received: $rec Transmitted: $sent"
+
+  is_connected=$(ping -c 1 8.8.8.8 | grep '1 received' | wc -l)
+
+  if [ $is_connected = '1' ]; then echo "Internet connection: True"; else echo  "Internet connection: False"; fi
+
+}
+
 display_mem_res
+display_net_info
