372b7f62e6fe1f176ae59e94bb313bcc327b4926
Mikey <54919721+mikebrownie@users.noreply.github.com>
2021-09-06 09:35:44 -0600

    Update monitor.sh
    Added parameter

--- labs/1_shell-scripting/monitor.sh
+++ labs/1_shell-scripting/monitor.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [ "$#" != "1" ]; then echo "Required arg: threshold percent of drive usage for email notification (0-100)"; exit -1; fi
+
+THRESHOLD=$1
+
 ROOT_USAGE=$(df | grep "/$" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//')
 
 BOOT_USAGE=$(df | grep "/boot" | awk '{print $5,$6}' | awk '{print $1}' | sed 's/%//')
@@ -7,13 +11,13 @@ i=0
 
 while [ $i -lt 100 ]
 do
-  if [ $BOOT_USAGE -gt 80 ]
+  if [ $BOOT_USAGE -gt $THRESHOLD ]
   then
-    echo "/boot usage at $BOOT_USAGE" | mailx -s "ALERT" root@localhost
+    echo "/boot usage $THRESHOLD exceeded: at $BOOT_USAGE" | mailx -s "ALERT" root@localhost
   fi
-  if [ $ROOT_USAGE -gt 80 ]
+  if [ $ROOT_USAGE -gt $THRESHOLD ]
   then
-    echo "root / usage at $ROOT_USAGE" | mailx -s "ALERT" root@localhost
+    echo "root / usage $THRESHOLD exceeded: at $ROOT_USAGE" | mailx -s "ALERT" root@localhost
   fi
   sleep 15
   i=`expr $i + 1`;
