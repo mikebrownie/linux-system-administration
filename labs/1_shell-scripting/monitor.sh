0bccea58865ee0e0b3b484bca402791e13b550a4
Mikey <54919721+mikebrownie@users.noreply.github.com>
2021-09-06 07:45:09 -0600

    Added mail

--- labs/1_shell-scripting/monitor.sh
+++ labs/1_shell-scripting/monitor.sh
@@ -5,17 +5,16 @@ BOOT_USAGE=$(df | grep "/boot" | awk '{print $5,$6}' | awk '{print $1}' | sed 's
 
 i=0
 
-while [ $i -lt 1000 ]
+while [ $i -lt 100 ]
 do
   if [ $BOOT_USAGE -gt 80 ]
   then
-    echo "Uh oh"
+    echo "/boot usage at $BOOT_USAGE" | mailx -s "ALERT" root@localhost
   fi
   if [ $ROOT_USAGE -gt 80 ]
   then
-    echo "Uh oh"
+    echo "root / usage at $ROOT_USAGE" | mailx -s "ALERT" root@localhost
   fi
   sleep 15
   i=`expr $i + 1`;
 done;
-
